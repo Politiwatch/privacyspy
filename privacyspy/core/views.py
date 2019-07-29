e87f483fe7ff850e269e80432c579e7a6bd3670d
R. Miles McCain <x1-key@sendmiles.email>
2019-07-28 21:34:25 -0400

    Expand views, use slug for products

--- privacyspy/core/views.py
+++ privacyspy/core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 def index(request):
-    return render(request, 'core/base.html', context={
-        "title": "Making online privacy (slightly) simpler"
+    return render(request, 'core/index.html', context={
+        "title": "Making online privacy (slightly) simpler",
+        "products": Product.objects.all()
     })
 
-def product(request, product_id):
+def product(request, product_slug):
     return render(request, 'core/base.html')
