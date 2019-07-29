d092369ae518006d819cbb33e2fd0c25ea3ce5c9
R. Miles McCain <x1-key@sendmiles.email>
2019-07-28 21:33:44 -0400

    Register models

--- privacyspy/core/admin.py
+++ privacyspy/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product, PrivacyPolicy, RubricQuestion, RubricOption, RubricSelection, Edit, SuggestedAction, Profile
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(PrivacyPolicy)
+admin.site.register(RubricQuestion)
+admin.site.register(RubricOption)
+admin.site.register(RubricSelection)
+admin.site.register(Edit)
+admin.site.register(SuggestedAction)
+admin.site.register(Profile)
