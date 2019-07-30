88ab584a9e461bb054282c57b50affb5bb3ef56c
ibarakaiev <ibarakaiev@gmail.com>
2019-07-30 22:18:32 +0300

    Redesign + laggy landing page

--- privacyspy/core/views.py
+++ privacyspy/core/views.py
@@ -3,11 +3,15 @@ from .models import Product, PrivacyPolicy, RubricQuestion, RubricSelection, Rub
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
 
+
 def index(request):
     return render(request, 'core/index.html', context={
         "title": "Making online privacy (slightly) simpler",
-        "products": Product.objects.all()
+        "n": range(60),
+        "keywords": ["privacy", "is", "a",
+                     "fundamental", "right"]
     })
+
 
 def product(request, product_slug):
     product = get_object_or_404(Product, slug=product_slug)
@@ -22,6 +26,7 @@ def product(request, product_slug):
         "policy": policy
     })
 
+
 @user_passes_test(lambda u: u.is_superuser)
 def edit_policy(request, policy_id):
     policy = get_object_or_404(PrivacyPolicy, id=policy_id)
@@ -31,31 +36,41 @@ def edit_policy(request, policy_id):
         print(request.POST)
         section = request.POST.get("section", None)
         if section == "metadata":
-            policy.out_of_date = request.POST.get("out-of-date", policy.out_of_date) == "True"
-            policy.erroneous = request.POST.get("erroneous", policy.erroneous) == "True"
-            policy.original_url = request.POST.get("original-url", policy.original_url)
-            policy.published = request.POST.get("published", policy.published) == "True"
+            policy.out_of_date = request.POST.get(
+                "out-of-date", policy.out_of_date) == "True"
+            policy.erroneous = request.POST.get(
+                "erroneous", policy.erroneous) == "True"
+            policy.original_url = request.POST.get(
+                "original-url", policy.original_url)
+            policy.published = request.POST.get(
+                "published", policy.published) == "True"
             policy.save()
             actions.append("Successfully updated metadata.")
         if section == "highlight-by-url":
             url = request.POST.get("source-url", None)
             if url != None:
                 policy.load_highlights_via_url(url)
-                actions.append("Loading highlights via the given URL in a background thread.")
+                actions.append(
+                    "Loading highlights via the given URL in a background thread.")
         if section == "highlight-by-plaintext":
             text = request.POST.get("source-text", None)
             if text != None:
                 policy.load_highlights_via_plaintext(text)
-                actions.append("Loading highlights via plaintext in a background thread.")
+                actions.append(
+                    "Loading highlights via plaintext in a background thread.")
         if section == "ratings":
             for question in policy.questions_with_selections():
-                option_str = request.POST.get(str(question.id) + '-selection', "None")
+                option_str = request.POST.get(
+                    str(question.id) + '-selection', "None")
                 if option_str != "None":
-                    option = get_object_or_404(RubricOption, id=int(option_str))
-                    citation = request.POST.get(str(question.id) + "-citation", "")
+                    option = get_object_or_404(
+                        RubricOption, id=int(option_str))
+                    citation = request.POST.get(
+                        str(question.id) + "-citation", "")
                     note = request.POST.get(str(question.id) + "-note", "")
                     if question.answer == None:
-                        RubricSelection.objects.create(option=option, policy=policy, citation=citation)
+                        RubricSelection.objects.create(
+                            option=option, policy=policy, citation=citation)
                     else:
                         if not (question.answer.option == option and question.answer.citation == citation and question.answer.note == note):
                             question.answer.option = option
@@ -66,7 +81,7 @@ def edit_policy(request, policy_id):
                     if question.answer != None:
                         question.answer.delete()
             actions.append("Successfully updated ratings.")
-            
+
     return render(request, 'core/edit_policy.html', context={
         "policy": policy,
         "title": "Editing Privacy Policy",
