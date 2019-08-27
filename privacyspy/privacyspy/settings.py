dd5ace729611be72559e43b1fd7ac7862f452ee5
R. Miles McCain <x1-key@sendmiles.email>
2019-08-26 20:44:17 -0400

    Add email settings

--- privacyspy/privacyspy/settings.py
+++ privacyspy/privacyspy/settings.py
@@ -110,6 +110,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS=True
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
