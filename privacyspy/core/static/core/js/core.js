894f5abd21f66282dc86666cf2d0662035a1e7da
R. Miles McCain <x1-key@sendmiles.email>
2019-09-01 15:06:59 -0400

    Add browser extension links

--- privacyspy/core/static/core/js/core.js
+++ privacyspy/core/static/core/js/core.js
@@ -46,3 +46,36 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
+
+// From https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser?rq=1
+
+// Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+
+// Chrome 1 - 71
+var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+function installBrowserExtension(){
+  if(isChrome){
+    window.open('https://chrome.google.com/webstore/detail/ppembnadnhiknioggbglgiciihgmkmnd', '_blank'); 
+  }else if(isFirefox){
+    window.open('https://addons.mozilla.org/en-US/firefox/addon/privacyspy/', '_blank'); 
+  }else{
+    alert("We can't detect your browser! To install the PrivacySpy extension, please navigate to the appropriate extension marketplace for your browser and install it.");
+  }
+}
