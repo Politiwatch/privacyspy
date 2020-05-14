fbb68bcc0149a6ec423810130c862d28145b5c94
R. Miles McCain <x1-key@sendmiles.email>
2020-05-14 13:32:25 -0400

    Update personal links

--- privacyspy/core/templatetags/core_tags.py
+++ privacyspy/core/templatetags/core_tags.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.simple_tag(name="credit", takes_context=False)
 def credit():
-    credit = ["<a href='https://rmrm.io'>Miles McCain</a>",
-              "<a href='https://github.com/ibarakaiev'>Igor Barakaiev</a>"]
+    credit = ["<a href='https://miles.land'>Miles McCain</a>",
+              "<a href='https://igor.fyi'>Igor Barakaiev</a>"]
     random.shuffle(credit)
     return ", ".join(credit)
