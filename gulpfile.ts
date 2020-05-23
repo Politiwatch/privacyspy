174f6a67e02325fc437f5d1ea06a684fc5bbb18a
Igor Barakaiev <ibarakaiev@gmail.com>
2020-05-23 23:20:31 +0300

    Landing page setup

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,33 +3,59 @@ let mustache = require("gulp-mustache");
 let postcss = require("gulp-postcss");
 let purgecss = require("gulp-purgecss");
 let del = require("del");
-gulp.task("cleanDist", async () => {
-  return del("./dist");
-})
 
-gulp.task("buildTemplates", async () => {
+gulp.task("cleanDist", () => {
+  return del("./dist");
+});
+
+gulp.task("buildTemplates", () => {
   return gulp
     .src(["./src/templates/**/*.html"])
-    .pipe(
-      mustache({})
-    )
+    .pipe(mustache({}))
     .pipe(gulp.dest("./dist"));
 });
 
-gulp.task("collectStatic", async () => {
-  return gulp.src(["./src/static/**/*", "!./src/static/**/*.css"]).pipe(gulp.dest("./dist/static/"))
+gulp.task("collectStatic", () => {
+  return gulp
+    .src(["./src/static/**/*", "!./src/static/**/*.css"])
+    .pipe(gulp.dest("./dist/static/"));
 });
 
-gulp.task("buildCss", async () => {
+gulp.task("buildCss", () => {
   return gulp
     .src(["./src/static/css/**/*.css"])
-    .pipe(postcss([require("postcss-import"), require("tailwindcss"), require("autoprefixer")]))
     .pipe(
-      purgecss({
-        content: ["./dist/**/*.html"],
-      })
+      postcss([
+        require("postcss-import"),
+        require("tailwindcss")("tailwind.config.js"),
+        require("autoprefixer"),
+      ])
     )
     .pipe(gulp.dest("./dist/static/css/"));
 });
 
-gulp.task("default", gulp.series(["cleanDist", "buildTemplates", "collectStatic", "buildCss"]));
+gulp.task("purge", () => {
+  return gulp.src(["./dist/static/css/**/*.css"]).pipe(
+    purgecss({
+      content: ["./dist/**/*.html"],
+    })
+  );
+});
+
+gulp.watch("src/templates/**/*.html", () => {
+  return gulp
+    .src(["./src/templates/**/*.html"])
+    .pipe(mustache({}))
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task(
+  "default",
+  gulp.series([
+    "cleanDist",
+    "buildTemplates",
+    "collectStatic",
+    "buildCss",
+    "purge",
+  ])
+);
