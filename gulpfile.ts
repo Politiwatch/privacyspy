0beec38a8ea0ae85807e679fb55e04a36533c0b2
R. Miles McCain <development-machine@sendmiles.email>
2020-05-24 00:35:27 +0000

    Add product page templates with outputs

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,52 +1,100 @@
-let gulp = require("gulp");
-let handlebars = require('gulp-hb');
-let postcss = require("gulp-postcss");
-let rename = require('gulp-rename');
-let del = require("del");
+import { Product, RubricQuestion } from "./src/parsing/types";
+import { loadRubric, loadProducts } from "./src/parsing/index";
 
-gulp.task("cleanDist", () => {
+let gulp = require("gulp");
+let postcss = require("gulp-postcss");
+let rename = require("gulp-rename");
+let del = require("del");
+let hb = require("gulp-hb");
+
+function hbsFactory(additionalData: object): any {
+  return hb()
+    .partials("./src/templates/partials/**/*.hbs")
+    .data({
+      rubric: rubric,
+      products: products,
+      ...additionalData,
+    });
+}
+
+let rubric: RubricQuestion[] = loadRubric();
+let products: Product[] = loadProducts(rubric);
+
+gulp.task("clean", () => {
   return del("./dist/**/*");
 });
 
-gulp.task("buildTemplates", () => {
+gulp.task("build general pages", () => {
   return gulp
-    .src(["./src/templates/**/*.hbs"])
-    .pipe(handlebars().partials("./src/templates/partials/**/*.hbs"))
-    .pipe(rename({extname: ".html"}))
-    .pipe(gulp.dest("./dist"));
+    .src("./src/templates/**/*.hbs", {
+      ignore: "./src/templates/product.hbs",
+    })
+    .pipe(hbsFactory({}))
+    .pipe(rename({ extname: ".html" }))
+    .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task("collectStatic", () => {
+// Product page building
+// TODO: Make a bit nicer
+let productPageBuildTasks = [];
+for (let product of products) {
+  let taskName = `build ${product.slug}`;
+  gulp.task(taskName, () => {
+    return gulp
+      .src("./src/templates/product.hbs")
+      .pipe(hbsFactory({ product: product }))
+      .pipe(rename(`/product/${product.slug}/index.html`))
+      .pipe(gulp.dest("./dist/"));
+  });
+  productPageBuildTasks.push(taskName);
+}
+
+gulp.task(
+  "build pages",
+  gulp.parallel(...productPageBuildTasks, "build general pages")
+);
+
+gulp.task("collect static", () => {
   return gulp
-    .src(["./src/static/**/*", "!./src/static/**/*.css"])
+    .src(["./src/static/**/*", "!./src/static/**/*.{css,scss}"])
     .pipe(gulp.dest("./dist/static/"));
 });
 
-gulp.task("buildCss", () => {
-  return gulp
-    .src(["./src/static/css/**/*.css"])
-    .pipe(
-      postcss([
-        require("postcss-import"),
-        require("tailwindcss")("tailwind.config.js"),
-        require("autoprefixer"),
-        // require("@fullhuman/postcss-purgecss")({
-        //   content: ["./dist/**/*.html"],
-        // })
-      ])
-    ).pipe(gulp.dest("./dist/static/css/"));
+gulp.task("collect product icons", () => {
+  return gulp.src(["./icons/**/*"]).pipe(gulp.dest("./dist/static/icons/"));
 });
 
-gulp.watch(["src/templates/**/*.hbs", "rubric/", "products/"], () => {
-  return gulp.series(["buildTemplates"]);
+gulp.task("build css", () => {
+  return gulp
+    .src(["./src/static/css/base.scss"])
+    .pipe(
+      postcss(
+        [
+          require("postcss-import"),
+          require("tailwindcss")("tailwind.config.js"),
+          require("autoprefixer"),
+          // require("@fullhuman/postcss-purgecss")({
+          //   content: ["./dist/**/*.html"],
+          // })
+        ],
+        { syntax: require("postcss-scss") }
+      )
+    )
+    .pipe(rename({ extname: ".css" }))
+    .pipe(gulp.dest("./dist/static/css/"));
 });
+
+// gulp.watch(["src/templates/**/*.hbs", "rubric/", "products/"], () => {
+//   return gulp.series(["build pages"]);
+// });
 
 gulp.task(
   "default",
   gulp.series([
-    "cleanDist",
-    "buildTemplates",
-    "collectStatic",
-    "buildCss",
+    "clean",
+    "build pages",
+    "collect static",
+    "collect product icons",
+    "build css",
   ])
 );
