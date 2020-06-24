3abca9e70a7be8b715bf201e73e15f2dc83e1f25
R. Miles McCain <development-machine@sendmiles.email>
2020-06-24 04:21:24 +0000

    Update testing system

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,3 @@
 module.exports = {
-    reporters: ["default", ["jest-junit", {
-        suiteName: "Automated Tests",
-        classNameTemplate: (vars) => {
-            let affectedFile = vars.classname.match(/\[(.+)\]/);
-            if (affectedFile != null) {
-                return affectedFile[1];
-            } else {
-                return vars.filepath;
-            }
-        },
-        titleTemplate: (vars) => {
-            return (vars.classname.replace(/\[(.+)\]/, "") + " " + vars.title).replace(/^\s+/, "");
-        }
-    }]
-    ]
+    reporters: ["<rootDir>/src/tests/reporter.js", "default"]
 };
