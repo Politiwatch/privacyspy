2e180b490b34497d1b902e5802d0d56f75f1d87b
R. Miles McCain <development-machine@sendmiles.email>
2020-06-24 04:21:24 +0000

    Update testing system

---
+++ src/tests/reporter.js
@@ -0,0 +1,25 @@
+class Reporter {
+  constructor(globalConfig, options) {
+    this._globalConfig = globalConfig;
+    this._options = options;
+  }
+
+  onRunComplete(contexts, results) {
+    if (process.env.TEST_ENVIRONMENT === "github") {
+      for (let suite of results.testResults) {
+        for (let test of suite.testResults) {
+          if (test.status !== "passed") {
+            let fileName = test.fullName.match(/\[(.+)\]/);
+            if (fileName !== null) {
+              fileName = fileName[1];
+            }
+            let error = test.fullName.replace(/\[(.+)\]/, "").replace(/^\s+/, "");
+            console.log(`::error ${fileName !== null ? "file=" + fileName : ""}::${error}`);
+          }
+        }
+      }
+    }
+  }
+}
+
+module.exports = Reporter;
