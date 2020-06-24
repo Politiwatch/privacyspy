b533bd7c6f6532f08ef04759164b348082454548
R. Miles McCain <development-machine@sendmiles.email>
2020-06-24 05:48:50 +0000

    Add line-level test results

--- src/tests/product.test.ts
+++ src/tests/product.test.ts
@@ -6,21 +6,26 @@ import fs from "fs";
 const rubric: RubricQuestion[] = loadRubric();
 const products: Product[] = loadProducts(rubric, loadContributors());
 
+// What's going on with these test descriptions? We have a homegrown system
+// to provide line-level annotations for our product TOML files.
+// The file name goes in the square brackets, and what to search for (regex)
+// to find the line numbers goes in the curly brackets.
+
 for (const product of products) {
   describe(`[products/${product.slug}.toml] Product '${product.name}'`, () => {
-    test(`must have a unique slug`, () => {
+    test(`must have a unique slug {slug}`, () => {
       expect(products.filter((p) => p.slug === product.slug).length).toEqual(1);
     });
 
-    test(`must have contributors listed`, () => {
+    test(`must have contributors listed {contributors}`, () => {
       expect(product.contributors.length).toBeGreaterThan(0);
     });
 
-    describe(`must have a valid description that is a full sentence`, () => {
+    describe(`must have a valid description that is a full sentence {description}`, () => {
       isMinFullSentence(product.description);
     });
 
-    describe(`must have hostname(s)`, () => {
+    describe(`must have hostname(s) {hostnames}`, () => {
       test("that exist", () => {
         expect(product.hostnames.length).toBeGreaterThan(0);
       });
@@ -44,25 +49,26 @@ for (const product of products) {
         expect(fs.statSync("icons/" + product.icon).size).toBeLessThan(30000);
       });
     });
+
     if (product.parent != null) {
       describe(`is a child product`, () => {
-        test(`so it must not have any sources (use parent product instead)`, () => {
+        test(`so it must not have any sources (use parent product instead) {sources}`, () => {
           expect(product.sources.length).toBe(0);
         });
-        test(`so it must not be graded (use parent product instead)`, () => {
+        test(`so it must not be graded (use parent product instead) {rubric}`, () => {
           expect(product.rubric.length).toBe(0);
         });
-        test(`so it must not have any updates (use parent product instead)`, () => {
+        test(`so it must not have any updates (use parent product instead) {updates}`, () => {
           expect(product.updates.length).toBe(0);
         });
       });
     } else {
-      test(`must have link(s) to the original policy`, () => {
+      test(`must have link(s) to the original policy (sources) {sources}`, () => {
         expect(product.sources.length).toBeGreaterThan(0);
       });
 
       for (const question of rubric) {
-        describe(`question '${question.slug}'`, () => {
+        describe(`question '${question.slug}' {rubric.${question.slug}}`, () => {
           const selection = product.rubric.find(
             (item) => item.question.slug === question.slug
           );
@@ -80,7 +86,7 @@ for (const product of products) {
       }
 
       for (const update of product.updates) {
-        describe(`update '${update.title}'`, () => {
+        describe(`update '${update.title} {[[updates]]}'`, () => {
           test("must have a title", () => {
             expect(update.title.length).toBeGreaterThan(0);
           });
--- src/tests/reporter.js
+++ src/tests/reporter.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 class Reporter {
   constructor(globalConfig, options) {
     this._globalConfig = globalConfig;
@@ -9,12 +11,25 @@ class Reporter {
       for (let suite of results.testResults) {
         for (let test of suite.testResults) {
           if (test.status !== "passed") {
-            let fileName = test.fullName.match(/\[(.+)\]/);
-            if (fileName !== null) {
-              fileName = fileName[1];
+            let fileNameSearcher = test.fullName.match(/\[(.+)\]/);
+            let lineSearcher = test.fullName.match(/{(.+)}/);
+            let fileName = null;
+            let lineNumber = 1;
+            if (fileNameSearcher !== null) {
+              fileName = fileNameSearcher[1];
+              if (lineSearcher !== null) {
+                let search = lineSearcher[1];
+                let file = fs.readFileSync(fileName, {
+                  encoding: "utf8"
+                });
+                let innerFileSearch = file.match(search);
+                if (innerFileSearch !== null) {
+                  lineNumber = file.slice(0, innerFileSearch.index).split("\n").length;
+                }
+              }
             }
-            let error = test.fullName.replace(/\[(.+)\]/, "").replace(/^\s+/, "");
-            console.error(`Error file=${fileName || ""} message=${error}`);
+            let error = test.fullName.replace(/\[(.+)\]/, "").replace(/{(.+)}/, "").replace(/^\s+/, "").replace(/\s\s+/, " ");
+            console.error(`Error file=${fileName || ""} line=${lineNumber} message=${error}`);
           }
         }
       }
