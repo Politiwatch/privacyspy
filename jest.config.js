45f98d53a3e6b741b198ee4a5dad29cf0e284c90
R. Miles McCain <development-machine@sendmiles.email>
2020-06-24 03:36:33 +0000

    Add file descriptors

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-    reporters: ["default", "jest-junit"]
+    reporters: ["default", ["jest-junit", {
+        suiteName: "Automated Tests",
+        classNameTemplate: (vars) => {
+            let affectedFile = vars.classname.match(/\[(.+)\]/);
+            if (affectedFile != null) {
+                return affectedFile[1];
+            } else {
+                return vars.filepath;
+            }
+        },
+        titleTemplate: (vars) => {
+            return (vars.classname.replace(/\[(.+)\]/, "") + " " + vars.title).replace(/^\s+/, "");
+        }
+    }]
+    ]
 };
